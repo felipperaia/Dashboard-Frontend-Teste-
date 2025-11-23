306a4d580ce3fcea7a660c9698741bee43fbc3d1
Felippe Adriel <felippeadriel46@gmail.com>
2025-11-23 17:25:40 -0300

    dfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 import Notifications from "./pages/Notifications";
 import MFA from "./pages/MFA";
-import Profile from "./pages/Profile";
 import Chat from "./pages/Chat";
 
 function AppContent({ pushEnabled, unsubscribeAndLogout }) {
@@ -32,8 +31,7 @@ function AppContent({ pushEnabled, unsubscribeAndLogout }) {
         <Route path="/settings" element={<Settings />} />
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/mfa" element={<MFA />} />
-        <Route path="/profile" element={<Profile />} />
-          <Route path="/chat" element={<Chat />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </>
   );
