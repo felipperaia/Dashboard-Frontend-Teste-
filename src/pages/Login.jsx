157189e135617e1d180c54e8e545c0adc21fead8
Felippe Adriel <felippeadriel46@gmail.com>
2025-11-23 19:35:31 -0300

    sdfsdfsdf

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.png";
 
 
 export default function Login({ onLoginSuccess }) {
